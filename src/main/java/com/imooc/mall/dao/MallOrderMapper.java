3291e4a54a6f555313aa299949705575d2653cad
NikHuang <nikhhq@sina.cn>
2019-12-31 16:48:42 +0800

    m3

---
+++ src/main/java/com/imooc/mall/dao/MallOrderMapper.java
@@ -0,0 +1,18 @@
+package com.imooc.mall.dao;
+
+import com.imooc.mall.domain.MallOrder;
+
+
+public interface MallOrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(MallOrder record);
+
+    int insertSelective(MallOrder record);
+
+    MallOrder selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(MallOrder record);
+
+    int updateByPrimaryKey(MallOrder record);
+}
